b7d208650e7f853824700e91acaeacb8bff3244c
Ram Hemasri <rhemasri@live.co.uk>
2019-02-04 11:50:58 +0000

    Added player die roll to move 4 spaces scenario

--- Katas.SnakeAndLadders.Tests/MovesAreDeterminedByDiceRolls.cs
+++ Katas.SnakeAndLadders.Tests/MovesAreDeterminedByDiceRolls.cs
@@ -1,5 +1,6 @@
-﻿using Xbehave;
+﻿using FakeItEasy;
 using FluentAssertions;
+using Xbehave;
 
 namespace Katas.SnakeAndLadders.Tests
 {
@@ -7,16 +8,16 @@ namespace Katas.SnakeAndLadders.Tests
     /// I want to move my token based on the roll of a die
     /// So that there is an element of chance in the game
     public class MovesAreDeterminedByDiceRolls
-    {        
+    {
         [Scenario]
         public void PlayerRollsDie_ShouldBeInRangeOf1And6(GameEngine gameEngine, Player player, IDice dice, int diceRollResult)
         {
-           
+
             "Given the game is started"
                 .x(() =>
                 {
                     dice = new Dice();
-                    player = new Player(1, "Player 1",dice);
+                    player = new Player(1, "Player 1", dice);
                     int totalSquares = 100;
                     gameEngine = new GameEngine(totalSquares);
                     gameEngine.AddPlayer(player);
@@ -24,7 +25,8 @@ namespace Katas.SnakeAndLadders.Tests
                 });
 
             "When the player rolls a die"
-                .x(() => {
+                .x(() =>
+                {
                     diceRollResult = player.RollDice();
                 });
 
@@ -34,6 +36,38 @@ namespace Katas.SnakeAndLadders.Tests
                  diceRollResult.Should().BeInRange(1, 6);
              });
 
+        }
+
+
+        [Scenario]
+        public void WhenPlayerRolls4_ThenTokenShouldMove4Spaces(GameEngine gameEngine, Player player,
+            IDice dice, int diceRollResult, int positionBeforeMove)
+        {
+            "Given the player rolls a 4"
+                .x(() =>
+                {
+                    dice = A.Fake<IDice>();
+                    A.CallTo(() => dice.Roll()).Returns(4);
+                    player = new Player(1, "Player 1", dice);
+                    int totalSquares = 100;
+                    gameEngine = new GameEngine(totalSquares);
+                    gameEngine.AddPlayer(player);
+                    gameEngine.Start();
+                });
+
+            "When they move their token"
+                .x(() =>
+                {
+                    positionBeforeMove = gameEngine.CurrentSquareForPlayer(player);
+                    gameEngine.MovePlayer(player, player.RollDice());
+                });
+            "Then the token should move 4 space"
+                .x(() =>
+                {
+                    int positionAfterMove = gameEngine.CurrentSquareForPlayer(player);
+                    int result = positionAfterMove - positionBeforeMove;
+                    result.Should().Be(4);
+                });
         }
 
     }
